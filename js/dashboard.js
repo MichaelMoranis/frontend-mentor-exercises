d80d800e04686475748215400480d085c2120aa6
michaelmoranis <michaelmoranesul@gmail.com>
2023-10-22 18:10:05 -0300

    concluindo verificacao com js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,24 +1,40 @@
 const form = document.querySelector("form");
 const dashboard = document.querySelector(".dashboard");
 const main = document.querySelector("main");
-const input = document.querySelector("input").value;
+const input = document.querySelector("input");
 const buttonDashboard = document.querySelector(".dissmiss")
+const sucessMensage = document.querySelector(".dashboard__paragraf");
+const p = document.createElement("p");
+const regexEmailGmail = /^[\w.-]+@gmail\.com$/;
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-    const sucessMensage = document.querySelector(".dashboard__paragraf");
-    dashboard.classList.remove("none");
-    dashboard.classList.add("block");
-    main.classList.add("none");
-    sucessMensage.innerHTML = `A confirmation email has been sent to
-    ${input}. Please open it and click the button inside to
-    confirm your subscription.`;
-    console.log("form capturado com sucesso")
-    input.innerHTML = "";
+
+    if(regexEmailGmail.test(input.value) != true) {
+        p.textContent = "formato de email incorreto !!, digite seu email novamente !!"
+    } else if(input.value === "") {
+        p.innerHTML = "este campo nao pode ficar vazio";
+        p.style.backgroundColor = "red";
+        p.style.color = "black";
+        console.log("campo vazio")
+    }  else {
+        dashboard.classList.remove("none");
+        dashboard.classList.add("block");
+        main.classList.add("none");
+        sucessMensage.innerHTML = `Um email de confirmação foi enviado para
+        ${input.value}. Por favor, abra-o e clique no botão dentro para
+        Confirme sua inscrição.`;
+        console.log("form capturado com sucesso")
+        input.value = "";
+    }
+    p.style.backgroundColor = "red";
+    p.style.color = "black";
+    form.appendChild(p);
 })
 
 buttonDashboard.addEventListener("click", function() {
     main.classList.remove("none");
     dashboard.classList.remove("block");
     dashboard.classList.add("none");
+    input.value = "";
   })
