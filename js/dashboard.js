eb1944a549fc1243d0f8c6dc93d4c4f0643108af
michaelmoranis <michaelmoranesul@gmail.com>
2023-10-22 10:12:12 -0300

    adicionando funcao de voltar no botao do dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,24 @@
 const form = document.querySelector("form");
 const dashboard = document.querySelector(".dashboard");
 const main = document.querySelector("main");
+const input = document.querySelector("input").value;
+const buttonDashboard = document.querySelector(".dissmiss")
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
+    const sucessMensage = document.querySelector(".dashboard__paragraf");
     dashboard.classList.remove("none");
     dashboard.classList.add("block");
     main.classList.add("none");
+    sucessMensage.innerHTML = `A confirmation email has been sent to
+    ${input}. Please open it and click the button inside to
+    confirm your subscription.`;
     console.log("form capturado com sucesso")
+    input.innerHTML = "";
 })
+
+buttonDashboard.addEventListener("click", function() {
+    main.classList.remove("none");
+    dashboard.classList.remove("block");
+    dashboard.classList.add("none");
+  })
